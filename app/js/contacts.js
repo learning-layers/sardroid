45bd82d110fc5798e26a9027a7990bf8fd1063b5
Matti Jokitulppo <melonmanchan@gmail.com>
2015-11-20 15:46:45 +0200

    Remove cordova mock data hack

--- app/js/contacts.js
+++ app/js/contacts.js
@@ -11,52 +11,35 @@ var contacts = angular.module('contacts', ['ngCordova'])
 
 
         $scope.searchKeyPress = function(keyCode) {
-            console.log(keyCode);
             if ((keyCode === 66 || keyCode === 13) && typeof cordova !== 'undefined') {
                 cordova.plugins.Keyboard.close();
             }
-        }
+        };
 
         $scope.selectUser = function(selectedUser) {
             $state.go('userprofile', { user: selectedUser });
-        }
+        };
 });
 
 contacts.factory('contactsFactory', function($cordovaContacts) {
     return {
         getAllContacts: function() {
-            if (typeof cordova !== 'undefined') {
                 return $cordovaContacts.find({
                     fields: ['id', 'displayName', 'name', 'phoneNumbers', 'emails', 'photos']
                 })
-                    .then(function (allContacts) {
+                .then(function (allContacts) {
                         return allContacts.map(function (c) {
-                            return {
+                           return {
                                 "original": c,
                                 "displayName": c.displayName || c.emails[0].value,
-                                "number": c.phoneNumbers ? c.phoneNumbers[0] : 'N/A',
-                                "photo": c.photos ? c.photos[0].value : 'res/img/logo.png'
-                            }
-                        })
-                    })
-                    .catch(function (err) {
-                        return err;
-                    })
-            } else {
-                return new Promise(function(resolve, reject) {
-                    resolve( [ {
-                        "original": {"displayName": "Durr2"},
-                        "displayName": "Durr1",
-                        "number": 'N/A',
-                        "photo": 'res/img/logo.png'
-                    }, {
-                        "original": {"displayName": "Durr2"},
-                        "displayName": "Durr2",
-                        "number": 'N/A',
-                        "photo": 'res/img/logo.png'
-                    }]);
-                })
-            }
+                                "number": c.phoneNumbers ? c.phoneNumbers[0] ? c.phoneNumbers[0].value : 'N/A' : 'N/A',
+                                "photo": c.photos ? c.photos[0] ? c.photos[0].value : 'res/img/logo.png' : 'res/img/logo.png'
+                         }
+                      })
+                  })
+                .catch(function (err) {
+                  return err;
+                });
         }
     };
 });
