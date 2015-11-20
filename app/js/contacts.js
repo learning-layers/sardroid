0209adc56aad3b20e9ac9a4de871f026cc77577b
Matti Jokitulppo <melonmanchan@gmail.com>
2015-11-20 11:44:06 +0200

    I am the factory master

--- app/js/contacts.js
+++ app/js/contacts.js
@@ -1,25 +1,33 @@
 'use strict';
 
 var contacts = angular.module('contacts', ['ngCordova'])
-.controller('ContactsCtrl', function($scope, $cordovaContacts) {
+.controller('ContactsCtrl', function($scope, contactsFactory) {
+        contactsFactory.getAllContacts().then(function (results) {
+            $scope.contacts = results;
+        }).catch(function(err) {
+            console.log(err);
+        });
+});
 
-        $scope.getAllContacts = function() {
-            $cordovaContacts.find({
+
+contacts.factory('contactsFactory', function($cordovaContacts) {
+    return {
+        getAllContacts: function() {
+            return $cordovaContacts.find({
                 fields: ['id', 'displayName', 'name', 'phoneNumbers', 'emails', 'photos']})
-            .then(function (allContacts) {
-                console.log(JSON.stringify(allContacts));
-                $scope.contacts = allContacts.map(function(c) {
-                    return {
-                        "displayName": c.displayName || c.emails[0].value,
-                        "number":      c.phoneNumbers ? c.phoneNumbers[0] : 'N/A',
-                        "photo":       c.photos       ? c.photos[0].value : 'res/img/logo.png'
-                    }
+                .then(function (allContacts) {
+                    console.log(JSON.stringify(allContacts));
+                    return allContacts.map(function(c) {
+                        return {
+                            "displayName": c.displayName || c.emails[0].value,
+                            "number":      c.phoneNumbers ? c.phoneNumbers[0] : 'N/A',
+                            "photo":       c.photos       ? c.photos[0].value : 'res/img/logo.png'
+                        }
+                    })
                 })
+                .catch(function(err) {
+                    return err;
             })
-            .catch(function(err) {
-                console.log(err);
-            })
-        };
-
-      $scope.getAllContacts();
+        }
+    };
 });
