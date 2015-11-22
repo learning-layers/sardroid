af7d0f281b382e951cb870f9fd04b0ca4a2118d6
Matti Jokitulppo <melonmanchan@gmail.com>
2015-11-22 14:10:53 +0200

    Add incremental gulp builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var gulp   = require('gulp');
-var concat = require('gulp-concat');
-var gulpif = require('gulp-if');
+var gulp     = require('gulp');
+var concat   = require('gulp-concat');
+var gulpif   = require('gulp-if');
+var cached   = require('gulp-cached');
+var remember = require('gulp-remember');
 
 var del      = require('del');
 var minimist = require('minimist');
@@ -28,20 +30,22 @@ var options = minimist(process.argv.slice(2), knownOptions);
 gulp.task('build', ['scss', 'js', 'html', 'fonts', 'copy-res'], function() {});
 
 gulp.task('scss', function() {
-   return gulp.src('app/scss/app.scss')
+   return gulp.src('app/scss/*.scss')
+       .pipe(cached('scss'))
        .pipe(gulpif(options.env === 'development', sourcemaps.init()))
        .pipe(sass())
        .on('error', sass.logError)
        .pipe(gulpif(options.env !== 'development', minifyCSS()))
+       .pipe(remember('scss'))
        .pipe(concat('app.css'))
        .pipe(gulpif(options.env === 'development', sourcemaps.write()))
        .pipe(gulp.dest('./www/css/'))
        .pipe(reload({stream: true}))
 });
 
-
 gulp.task('html', function() {
     return gulp.src(['./app/**/*.html', '!./app/vendor/**/*.html'])
+       .pipe(cached('html'))
        .pipe(gulpif(options.env !== 'development', minifyHTML()))
        .pipe(gulp.dest('./www/'))
        .pipe(reload({stream: true}))
@@ -54,8 +58,6 @@ gulp.task('vendor-js', function() {
        { base: './app' })
        .pipe(gulp.dest('./www/'))
 });
-
-
 
 gulp.task('copy-res', function() {
    return gulp.src(['./app/res/**/*'],
@@ -70,6 +72,7 @@ gulp.task('fonts', function() {
 
 gulp.task('js', ['vendor-js'], function() {
     return gulp.src('./app/js/**/*')
+        .pipe(cached('js'))
         .pipe(gulp.dest('./www/js/'))
         .pipe(reload({stream: true}))
 });
@@ -79,11 +82,34 @@ gulp.task('watch', ['build'], function() {
         server: { baseDir: 'www/' },
         open:   false
     });
-    gulp.watch('app/scss/*.scss', ['scss'], {cwd: 'www'}, reload);
-    gulp.watch(['./app/**/*.html', '!./app/vendor/**/*.html'], ['html'], {cwd: 'www'}, reload);
-    gulp.watch('./app/js/**/*', ['js'], {cwd: 'www'}, reload);
-});
 
+    var scss = gulp.watch('app/scss/*.scss', ['scss'], {cwd: 'www'}, reload);
+    var html = gulp.watch(['./app/**/*.html', '!./app/vendor/**/*.html'], ['html'], {cwd: 'www'}, reload);
+    var js   = gulp.watch('./app/js/**/*', ['js'], {cwd: 'www'}, reload);
+
+    scss.on('change', function(event) {
+        if (event.type === 'deleted') {
+            console.log('Forgetting scss: ' + event.path);
+            delete cached.caches.scss[event.path];
+            remember.forget('scss', event.path);
+        }
+    });
+
+    html.on('change', function(event) {
+        if (event.type === 'deleted') {
+            console.log('Forgetting html: ' + event.path);
+            delete cached.caches.html[event.path];
+        }
+    });
+
+    js.on('change', function(event) {
+        if (event.type === 'deleted') {
+            console.log('Forgetting js: ' + event.path);
+            delete cached.caches.js[event.path];
+            remember.forget('js', event.path);
+        }
+    });
+});
 
 gulp.task('clean', function() {
     return del('www');
