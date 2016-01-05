ca84536816cc51515507a2d16b62216476ffc352
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-05 16:31:28 +0200

    Some registering controller functionality

--- app/js/controllers/register.js
+++ app/js/controllers/register.js
@@ -5,7 +5,25 @@
  */
 
 angular.module('register', [])
-.controller('RegisterCtrl', function($scope, $state, $localStorage, $http, configFactory) {
+.controller('RegisterCtrl', function($scope, $state, $localStorage, $translate, $http, $ionicPopup, configFactory) {
     var url = configFactory.getValue('apiUrl');
+
+    $scope.register = function (newUser) {
+
+        if (!newUser || !newUser.password || !newUser.passwordAgain || !newUser.code) {
+            return;
+        }
+
+        if (newUser.password !== newUser.passwordAgain) {
+            var callEndedAlert = $ionicPopup.alert({
+                title: $translate.instant('PASSWORD_ERROR'),
+                template:$translate.instant('PASSWORD_NO_MATCH') 
+            });
+            callEndedAlert.then(function() {
+                newUser.password      = "";
+                newUser.passwordAgain = "";
+            });
+        }
+    };
 });
 
