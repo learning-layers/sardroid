4ad42b972c3a677eaf7384dc658d94aa6ade6dd1
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-07 12:15:39 +0200

    Add functional logging in

--- app/js/controllers/login.js
+++ app/js/controllers/login.js
@@ -7,11 +7,13 @@
 
 angular.module('login', ['peerhandler'])
 
-.controller('LoginCtrl', function($scope, $state, $localStorage, $ionicHistory, peerFactory, socketFactory) {
+.controller('LoginCtrl', function($scope, $state, $localStorage, $ionicHistory, $http, peerFactory, socketFactory, configFactory) {
         // Disable back button so we can't back to login!
         $ionicHistory.nextViewOptions({
             disableBack: true
         });
+
+        var url = configFactory.getValue('apiUrl');
 
         // Hack so we're disconnected for sure!
         peerFactory.disconnectFromPeerJS();
@@ -25,13 +27,23 @@ angular.module('login', ['peerhandler'])
         }
 
         $scope.login = function(user) {
-            if (typeof user !== 'undefined' && user.phoneNumber) {
-                // Should probably do some back-end log in related stuff in here?
-                $localStorage.user = user;
-                peerFactory.connectToPeerJS(user.phoneNumber).then(function() {
-                    $state.go('tabs.contacts');
-                    socketFactory.connectToServer();
-                });
+            if (typeof user !== 'undefined' && user.phoneNumber && user.password) {
+
+                $http.post(
+                    url + 'auth/login',
+                    { phoneNumber : user.phoneNumber,
+                     password     : user.password }
+                ).then(function success(results) {
+                    console.log(results);
+                    $localStorage.user  = results.data.user;
+                    $localStorage.token = results.data.user.token;
+                    peerFactory.connectToPeerJS(user.phoneNumber).then(function() {
+                        $state.go('tabs.contacts');
+                        socketFactory.connectToServer();
+                    });
+                }, function error(err) {
+                    console.log(err);
+                })
             }
         };
 });
