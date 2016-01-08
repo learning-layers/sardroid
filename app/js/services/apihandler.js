cd8a3d11287f247fe3898593f80bf1bdf3840d4f
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-08 14:42:50 +0200

    Add error types from REST Api

--- app/js/services/apihandler.js
+++ app/js/services/apihandler.js
@@ -1,14 +1,33 @@
 'use strict';
 
 /*
- * Abstration layer for various RESTful api calls
+ * Abstration layer for various RESTful API calls
  */
 
 var apihandler = angular.module('apihandler', []);
 
 apihandler.factory('apiFactory', function ($http, configFactory) {
     // Private API
-    var url = configFactory.getValue('apiUrl')
+    var url = configFactory.getValue('apiUrl');
+
+    // Various different kinds of errors that can be returned from the REST API
+    var errorTypes = {
+        VERIFICATION   : {
+            USER_EXISTS    : 'user_exists',
+            NUMBER_MISSING : 'number_missing'
+        },
+        REGISTER       : {
+            NO_VERIFICATION      : 'no_verification',
+            VERIFICATION_EXPIRED : 'verification_expired',
+            VERIFICATION_USED    : 'verification_used',
+            REGISTER_FAILED      : 'register_failed',
+        },
+        LOGIN          : {
+            USER_NOT_FOUND : 'user_not_found',
+            BCRYPT_ERROR   : 'bcrypt_error',
+            WRONG_PASSWORD : 'wrong_password'
+        }
+    };
 
     // Public API
     return {};
