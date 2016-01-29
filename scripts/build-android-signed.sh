2acc6cd18e309434da21ca4e79e6f610b07ded62
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-29 15:34:07 +0200

    Add zipalign to signing script

--- scripts/build-android-signed.sh
+++ scripts/build-android-signed.sh
@@ -6,3 +6,6 @@ cordova build android --verbose --release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/aalto_android.keystore platforms/android/build/outputs/apk/android-x86-release-unsigned.apk google_play
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/aalto_android.keystore platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk google_play
 
+~/Android/Sdk/build-tools/23.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-x86-release-unsigned.apk SoARx86.apk
+~/Android/Sdk/build-tools/23.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk SoAR-ARM.apk
+
