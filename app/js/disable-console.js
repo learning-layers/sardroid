97d9a114579daf8e197a3bb17d6a6816dbe89cd9
Matti Jokitulppo <melonmanchan@gmail.com>
2016-02-08 21:31:46 +0200

    Actually disable console

--- app/js/disable-console.js
+++ app/js/disable-console.js
@@ -1,10 +1,11 @@
 (function() {
     'use strict';
     if (window.env.environment === 'production') {
-        var console   = {}
-        console.log   = function () {}
-        console.error = function () {}
-        console.warn  = function () {}
-        console.info  = function () {}
+        var console    = {};
+        console.log    = function () {};
+        console.error  = function () {};
+        console.warn   = function () {};
+        console.info   = function () {};
+        window.console = console;
     }
 })();
