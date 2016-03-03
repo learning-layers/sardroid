87983ddf46d0e61cbe6bbe55a9d2a784b0b5934b
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-03 12:13:35 +0200

    Start cleaning up modal handler a bit

--- app/js/services/modalhandler.js
+++ app/js/services/modalhandler.js
@@ -7,12 +7,33 @@
 var modalhandler = angular.module('modalhandler', []);
 
 modalhandler.factory('modalFactory', function($ionicPopup) {
+    // Keep tabs on currently active pop ups.
+    // Mostly so we don't spam them too often.
+
+    var currentPopups = [];
+
+    var pruneUsedPopupsByType = function (type) {
+        console.log('pruning used popups, old array: ', currentPopups);
+        currentPopups = _.reject(currentPopups, function (p) { return p.type === type; });
+        console.log('pruning used popups, new array: ', currentPopups);
+    };
+
     return {
         alert: function (title, template) {
-            return $ionicPopup.alert({
-                title    : title,
-                template : template
-            });
+
+            return new Promise(function (resolve, reject) {
+                var alertPopup = $ionicPopup.alert({
+                    title    : title,
+                    template : template
+                });
+
+                currentPopups.push({type: title, popupRef: alertPopup});
+
+                alertPopup.then(function () {
+                    pruneUsedPopupsByType(title)
+                    resolve();
+                })
+            })
         }
     }
 });
