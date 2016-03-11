35c925bfbdc1bec6406eb6ca464bdc6945e37349
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-11 15:59:50 +0200

    Refactor call controller

--- app/js/controllers/call.js
+++ app/js/controllers/call.js
@@ -6,97 +6,97 @@
  */
 
 angular.module('call', [])
+.controller('CallCtrl', function ($scope, $document, $sce, $stateParams, peerFactory, drawingFactory) {
 
-.controller('CallCtrl', function($scope, $sce, $stateParams, peerFactory, drawingFactory) {
+    var localWrapper  = $document[0].querySelector('#local-wrapper');
+    var remoteWrapper = $document[0].querySelector('#remote-wrapper');
 
-            if ($stateParams && $stateParams.user) {
-                    $scope.user = $stateParams.user;
-            } else {
-                    $scope.user = { displayName: '?????'};
+    var leave = function () {
+        peerFactory.sendDataToPeer({ type: 'otherPeerLeft' });
+        drawingFactory.tearDownDrawingFactory();
+        peerFactory.clearCallback('otherPeerLeft');
+        peerFactory.endCurrentCall();
+    };
+
+
+    var localStreamSrc = peerFactory.getLocalStreamSrc();
+    var remoteStreamSrc = peerFactory.getRemoteStreamSrc();
+
+    var localCanvas =  $document[0].querySelector('#local-canvas');
+    var remoteCanvas = $document[0].querySelector('#remote-canvas');
+
+
+    // Sweet hack for browser if you can't be bothered to make a call
+    if (localStreamSrc === null) { localStreamSrc = 'res/img/SampleVideo_1080x720_10mb.mp4'; }
+    if (remoteStreamSrc === null) { remoteStreamSrc = 'res/img/SampleVideo_1080x720_10mb.mp4'; }
+
+
+    drawingFactory.setUpDataCallbacks();
+    drawingFactory.setUpRemoteCanvas(remoteCanvas, {});
+    drawingFactory.setUpLocalCanvas(localCanvas, {});
+
+    peerFactory.registerCallback('otherPeerLeft', function () {
+        leave();
+    });
+
+    if ($stateParams && $stateParams.user) {
+        $scope.user = $stateParams.user;
+    } else {
+        $scope.user = { displayName: '?????' };
+    }
+
+    $scope.leave = leave;
+
+    $scope.determineFullscreenButtonClass = function () {
+        if ($scope.currentFullscreenCanvas) {
+            return 'ion-arrow-shrink';
+        }
+        return 'ion-arrow-expand';
+    };
+
+    $scope.localStreamSrc  = $sce.trustAsResourceUrl(localStreamSrc);
+    $scope.remoteStreamSrc = $sce.trustAsResourceUrl(remoteStreamSrc);
+
+    // TODO: Refactor this into something more elegant
+    $scope.toggleFullscreen = function (canvasId) {
+
+        if ($scope.currentFullscreenCanvas === canvasId) {
+
+            $scope.currentFullscreenCanvas = null;
+            drawingFactory.zoomOutCanvasByTag(canvasId);
+
+            switch (canvasId) {
+            case 'local':
+                localWrapper.classList.remove('fullscreen');
+                remoteWrapper.style.display = '';
+                break;
+            case 'remote':
+                remoteWrapper.classList.remove('fullscreen');
+                localWrapper.style.display = '';
+                break;
             }
 
-            var localWrapper  = document.querySelector('#local-wrapper');
-            var remoteWrapper = document.querySelector('#remote-wrapper');
+        } else {
 
+            drawingFactory.zoomInCanvasByTag(canvasId);
+            $scope.currentFullscreenCanvas = canvasId;
 
-            //TODO: Refactor this into something more elegant
-            $scope.toggleFullscreen = function (canvasId) {
-
-                if ($scope.currentFullscreenCanvas === canvasId) {
-
-                    $scope.currentFullscreenCanvas = null;
-                    drawingFactory.zoomOutCanvasByTag(canvasId);
-
-                    switch (canvasId) {
-                        case 'local':
-                            localWrapper.classList.remove('fullscreen');
-                            remoteWrapper.style.display = '';
-                        break;
-                        case 'remote':
-                            remoteWrapper.classList.remove('fullscreen');
-                            localWrapper.style.display = '';
-                        break;
-                    }
-
-                } else {
-
-                    drawingFactory.zoomInCanvasByTag(canvasId);
-                    $scope.currentFullscreenCanvas = canvasId;
-
-                    switch (canvasId) {
-                        case 'local':
-                           localWrapper.classList.add('fullscreen');
-                            remoteWrapper.style.display = 'none';
-                        break;
-                        case 'remote':
-                            remoteWrapper.classList.add('fullscreen');
-                            localWrapper.style.display = 'none';
-                        break;
-                    }
-                }
-
+            switch (canvasId) {
+            case 'local':
+                localWrapper.classList.add('fullscreen');
+                remoteWrapper.style.display = 'none';
+                break;
+            case 'remote':
+                remoteWrapper.classList.add('fullscreen');
+                localWrapper.style.display = 'none';
+                break;
             }
+        }
 
-            $scope.determineFullscreenButtonClass = function () {
-                if ($scope.currentFullscreenCanvas) {
-                    return 'ion-arrow-shrink'
-                } else {
-                    return 'ion-arrow-expand'
-                }
-            }
+    };
 
-            var leave = function () {
-                peerFactory.sendDataToPeer({ type: 'otherPeerLeft' });
-                drawingFactory.tearDownDrawingFactory();
-                peerFactory.clearCallback('otherPeerLeft');
-                peerFactory.endCurrentCall();
-            }
-
-            $scope.leave = leave;
-
-            var localStreamSrc = peerFactory.getLocalStreamSrc();
-            var remoteStreamSrc = peerFactory.getRemoteStreamSrc();
-
-            // Sweet hack for browser if you can't be bothered to make a call
-            if (localStreamSrc === null) { localStreamSrc = 'res/img/SampleVideo_1080x720_10mb.mp4' }
-            if (remoteStreamSrc === null) { remoteStreamSrc = 'res/img/SampleVideo_1080x720_10mb.mp4' }
-
-            var localCanvas = document.querySelector('#local-canvas');
-            var remoteCanvas = document.querySelector('#remote-canvas');
-
-            $scope.localStreamSrc  = $sce.trustAsResourceUrl(localStreamSrc);
-            $scope.remoteStreamSrc = $sce.trustAsResourceUrl(remoteStreamSrc);
-
-            drawingFactory.setUpDataCallbacks();
-            drawingFactory.setUpRemoteCanvas(remoteCanvas,{});
-            drawingFactory.setUpLocalCanvas(localCanvas, {});
-
-            peerFactory.registerCallback('otherPeerLeft', function (data) {
-                leave();
-            });
-
-            $scope.$on('$ionicView.leave', function(event) {
-                leave();
-            });
+    $scope.$on('$ionicView.leave', function () {
+        leave();
+    });
 });
 
