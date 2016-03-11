d6c0d3136522a741a0044f90e26e77fd235ecc73
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-11 16:29:55 +0200

    Refactor quit controller

--- app/js/controllers/quit.js
+++ app/js/controllers/quit.js
@@ -1,21 +1,19 @@
 'use strict';
+
 /*
  * Teensy controller for handling the quit button in the side menu
  */
 
 angular.module('quit', [])
-
-    .controller('QuitCtrl', function($scope, apiFactory) {
-        $scope.exit  = function() {
-
-            apiFactory.auth.logout()
-                .then(function (results) {
-                    navigator.app.exitApp();
-                })
-                .catch(function (error) {
-                    navigator.app.exitApp();
-                })
-
-        };
+.controller('QuitCtrl', function ($scope, apiFactory) {
+    $scope.exit  = function () {
+        apiFactory.auth.logout()
+            .then(function () {
+                navigator.app.exitApp();
+            })
+            .catch(function () {
+                navigator.app.exitApp();
+            });
+    };
 });
 
