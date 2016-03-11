76dff94ec5bab44348c3b9cc831bcdee587723bd
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-11 13:30:41 +0200

    Refactor contacts factory

--- app/js/services/contactshandler.js
+++ app/js/services/contactshandler.js
@@ -1,5 +1,8 @@
+'use strict';
 
-angular.module('contacts').factory('contactsFactory', function($cordovaContacts, $http, $localStorage, apiFactory, configFactory) {
+angular.module('contacts').factory('contactsFactory', function ($cordovaContacts, $http, $log,
+                                                               $localStorage, apiFactory) {
+
     // Array to store all the devices contacts so we don't have to re-fetch them all the time
     var contacts = [];
 
@@ -13,69 +16,106 @@ angular.module('contacts').factory('contactsFactory', function($cordovaContacts,
 
         contactStates: contactStates,
 
-        fetchAllContacts: function() {
+        fetchAllContacts: function () {
+
             // As far as I know, the fields do nothing. Cordova just YOLO's everything?
             var opts = {
                 fields: ['id', 'displayName', 'name', 'phoneNumbers', 'emails', 'photos'],
                 hasPhoneNumber : true
             };
 
-            return new Promise(function(resolve, reject) {
+            return new Promise(function (resolve, reject) {
+
                 $cordovaContacts.find(opts)
                     .then(function (allContacts) {
+
+                        var number;
+                        var displayName = 'Unknown';
+                        var photo = 'img/keilamies-small.png';
                         var userPhone = $localStorage.user.phoneNumber;
 
                         var formattedContacts = _.reduce(allContacts, function (formatted, c) {
-                            if (!(_.isEmpty(c.phoneNumbers)) && c.phoneNumbers.length > 0 && c.phoneNumbers[0].value.replace(/[+ ]/g, '') !== userPhone) {
-                                    var number = c.phoneNumbers[0].value.replace(/[+ ]/g, '');
-                                    var displayName = 'Unknown';
 
-                                    if (c.displayName) displayName = c.displayName;
-                                    else if (!_.isEmpty(c.emails)) displayName = c.emails[0].value
+                            if (!(_.isEmpty(c.phoneNumbers)) &&
+                                c.phoneNumbers.length > 0
+                                && c.phoneNumbers[0].value.replace(/[+ ]/g, '') !== userPhone) {
 
-                                    formatted.push({
-                                        "original"     : c,
-                                        "displayName"  : displayName,
-                                        "phoneNumber"  : number,
-                                        "photo"        : c.photos ? c.photos[0] ? c.photos[0].value : 'img/keilamies-small.png' : 'img/keilamies-small.png'
-                                    });
+                                number = c.phoneNumbers[0].value.replace(/[+ ]/g, '');
+
+                                if (c.displayName) displayName = c.displayName;
+                                else if (!_.isEmpty(c.emails)) displayName = c.emails[0].value;
+
+                                if (!_.isEmpty(c.photos) && angular.isDefined(c.photos[0].value)) {
+
+                                    photo = c.photos[0].value;
+
+                                }
+
+                                formatted.push({
+                                    original     : c,
+                                    displayName  : displayName,
+                                    phoneNumber  : number,
+                                    photo        : photo
+                                });
+
                             }
+
                             return formatted;
+
                         }, []);
 
                         resolve(formattedContacts);
+
                     })
                     .catch(function (err) {
-                        reject(err)
+
+                        reject(err);
+
                     });
-            })
+
+            });
+
         },
 
         syncContactsWithServer: function () {
+
             var self = this;
             return new Promise(function (resolve, reject) {
+
                 self.fetchAllContacts()
                     .then(function (results) {
-                        return apiFactory.user.contacts.updateContactsList(results)
+
+                        return apiFactory.user.contacts.updateContactsList(results);
+
                     })
                     .then(function (syncedContacts) {
+
                         $localStorage.contactsBeenSynced = true;
                         self.setContacts(syncedContacts);
                         resolve(syncedContacts);
+
                     })
                     .catch(function (err) {
-                        console.log('Error syncing contacts!', err);
+
+                        $log.log('Error syncing contacts!', err);
                         reject(err);
-                })
-            })
+
+                    });
+
+            });
+
         },
 
         fetchContactsFromServer: function () {
+
             return apiFactory.user.contacts.fetchContactsList();
+
         },
 
         addNewContact: function (newContact) {
+
             return new Promise(function (resolve, reject) {
+
                 $cordovaContacts.save(
                     {
                         phoneNumbers: [new ContactField('mobile', newContact.phoneNumber, true)],
@@ -83,48 +123,66 @@ angular.module('contacts').factory('contactsFactory', function($cordovaContacts,
                     }
                 )
                 .then(function (results) {
-                    console.log(results);
+
+                    $log.log(results);
                     resolve(results);
+
                 })
                 .catch(function (error) {
-                    console.log(error);
+
+                    $log.log(error);
                     reject(error);
+
                 });
-            })
+
+            });
+
         },
 
         setContacts: function (newContacts) {
+
             contacts = newContacts;
+
         },
 
         getContacts: function () {
+
             return contacts;
+
         },
 
         getContactByNumber: function (number) {
+
             return _.find(contacts, 'phoneNumber', number);
+
         },
 
         sortContactsByState: function () {
+
             contacts = _.sortBy(contacts, function (c) {
+
                 return c.currentState === contactStates.OFFLINE;
+
             });
+
         },
 
         setContactStateIfApplicable: function (number, state) {
-            console.log(contacts);
+
             var index = _.indexOf(contacts, this.getContactByNumber(number));
 
             if (index === -1) {
+
                 return false;
+
             }
 
-            else {
-                console.log('setting contact ' + number + ' state to ' + state);
-                contacts[index].currentState = state;
-                return true;
-            }
+            $log.log('setting contact ' + number + ' state to ' + state);
+            contacts[index].currentState = state;
+            return true;
+
         }
     };
+
 });
 
