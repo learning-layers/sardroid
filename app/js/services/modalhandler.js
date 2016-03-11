b2c90973933e48014f1ab62c431160e7d94bc8c4
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-11 14:00:28 +0200

    Refactor modalfactory

--- app/js/services/modalhandler.js
+++ app/js/services/modalhandler.js
@@ -4,75 +4,109 @@
  * Simple wrapper around $ionicPopup for modals and alerts and such
  */
 
-var modalhandler = angular.module('modalhandler', []);
+angular.module('modalhandler', [])
+.factory('modalFactory', function ($ionicPopup) {
 
-modalhandler.factory('modalFactory', function($ionicPopup) {
     // Keep tabs on currently active pop ups.
     // Mostly so we don't spam them too often.
 
     var currentPopups = [];
 
     var pruneUsedPopupsByType = function (type) {
-        return _.reject(currentPopups, function (p) { return p.type === type; });
+
+        return _.reject(currentPopups, function (p) {
+
+            return p.type === type;
+
+        });
+
     };
 
     var getPopupsByType = function (type) {
+
         return _.where(currentPopups, { type: type });
+
     };
 
     return {
         closeAllPopups: function () {
-            currentPopups.map(function (p) {
+
+            currentPopups.forEach(function (p) {
+
                 p.popupRef.close();
+
             });
 
             currentPopups = [];
+
         },
         alert: function (title, template) {
-            return new Promise(function (resolve, reject) {
+
+            return new Promise(function (resolve) {
+
+                var alertPopup;
+
                 if (getPopupsByType(title).length === 0) {
-                    var alertPopup = $ionicPopup.alert({
+
+                    alertPopup = $ionicPopup.alert({
                         title    : title,
                         template : template
                     });
-                    currentPopups.push({type: title, popupRef: alertPopup});
+
+                    currentPopups.push({ type: title, popupRef: alertPopup });
 
                     alertPopup.then(function () {
-                        currentPopups = pruneUsedPopupsByType(title)
+
+                        currentPopups = pruneUsedPopupsByType(title);
                         resolve();
-                    })
+
+                    });
+
                 } else {
+
                     // Same kind of popup is already visible, just resolve without spamming
                     resolve();
+
                 }
-            })
+
+            });
+
         },
 
         confirm: function (title, template) {
-            return new Promise(function (resolve, reject) {
+
+            return new Promise(function (resolve) {
+
+                var confirmPopup;
+
                 if (getPopupsByType(title).length === 0) {
 
-                    var confirmPopup = $ionicPopup.confirm({
+                    confirmPopup = $ionicPopup.confirm({
                         title    : title,
                         template : template
                     });
 
-                    currentPopups.push({type: title, popupRef: confirmPopup});
+                    currentPopups.push({ type: title, popupRef: confirmPopup });
 
                     confirmPopup.then(function (res) {
-                        console.log(res);
-                        currentPopups = pruneUsedPopupsByType(title)
+
+                        currentPopups = pruneUsedPopupsByType(title);
                         resolve(res);
-                    })
+
+                    });
 
                 } else {
+
                     // Same kind of popup is already visible, just resolve without spamming
                     resolve();
+
                 }
-            })
+
+            });
 
         }
 
-    }
+    };
+
 });
 
