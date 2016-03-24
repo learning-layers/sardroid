ab445054ffef49f29f81fd588605ea8a48004e9d
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-24 21:14:57 +0200

    Remove duplicate contacts syncing stuff from settings 

--- app/js/controllers/settings.js
+++ app/js/controllers/settings.js
@@ -9,16 +9,6 @@ angular.module('settings', [])
     $scope.settings = settingsFactory.getAllSettings();
     $scope.appVersion = $window.env.version;
 
-    $scope.updateContactsList = function () {
-        $ionicLoading.show({ template: $translate.instant('SYNCING_CONTACTS') });
-
-        return contactsFactory.syncContactsWithServer()
-            .then(function () {
-                $ionicLoading.hide();
-                $ionicHistory.goBack();
-            });
-    };
-
     $scope.toggleVideoSave = function (videoSaveState) {
         settingsFactory.setSettings({ saveCalls: videoSaveState });
     };
